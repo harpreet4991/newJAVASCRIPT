c4226a0722bfbe735a0db75adabf934750e90ab0
harpreet4991 <harpreet4991@gmail.com>
2023-11-02 15:15:56 +0530

    prog for condition

---
+++ java/index.js
@@ -0,0 +1,70 @@
+// var x=5
+// var y=8
+// var z=x+y
+// console.log(z)
+
+// var x=8
+// var y=5
+// var z=x-y
+// alert("subtraction result is "+z)
+
+// var a=4
+// var b=5
+// var c=a*b
+// alert("multiplication is "+c)
+
+
+// var x=10git 
+// var y=5
+// var z=x/y
+// console.log(z)
+
+
+// var different method
+
+// var x=100 
+//  x=50
+//  var y=50
+ 
+//  z=x+y
+//  console.log(z)
+
+
+// 2nd var method
+
+// var x=prompt("enter your first number")
+// var y=prompt("enter the second number")
+// var z=Number(x)+Number(y)
+// alert("your first number is " + x +" your second number is "+ y + " your total number is "+ z)
+
+
+// third method
+
+// var s=prompt("enter the money given by costomer")
+// var d=prompt("enter the price of thing")
+// var z=Number(s)-Number(d)
+// alert(" money given by costomer is "+s + " price of thing is" + d+ "rest money"+z)
+
+
+// let x=10
+// x=15
+// x=30
+// let y=5
+// y=10
+// let z=x+y
+// console.log(z)
+
+// const harpreet=10
+// const karam=8
+// const sidarth=harpreet+karam
+// console.log(sidarth)
+
+
+var age=prompt("enter your age")
+if(age>=18){
+    alert("you are eligible for vote")
+} else{
+    alert("you are not eligible for vote")
+}
+
+
